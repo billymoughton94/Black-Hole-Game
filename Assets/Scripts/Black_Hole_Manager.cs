52eaf478aa9a3acf903d3af3c092af48861441fc
billymoughton94 <billymoughton94@gmail.com>
2020-10-21 12:13:57 +0100

    First commit of game

---
+++ Assets/Scripts/Black_Hole_Manager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class Black_Hole_Manager : MonoBehaviour
+{
+    // amount of time (in seconds) needed for Black Hole to reach its maximum threshold
+    public int duration;
+
+    // starting size vs maximum size of Black Hole
+    private Vector3 startScale;
+    private Vector3 targetScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startScale = gameObject.transform.localScale;
+        targetScale = new Vector3(2000.0f, 2000.0f, 2000.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(increaseMass());
+    }
+
+    IEnumerator increaseMass()
+    {
+        float timeElapsed = 0;
+
+        // whilst timeElapsed <= duration, expand the Black Hole size per frame
+        while (timeElapsed <= duration)
+        {     
+            gameObject.transform.localScale = Vector3.Lerp(startScale, targetScale, timeElapsed / duration);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Camera_Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour
+{
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMovement = Input.GetAxis("Horizontal") * (moveSpeed * Time.deltaTime);
+        float zMovement = Input.GetAxis("Vertical") * (moveSpeed * Time.deltaTime);
+        Vector3 movement = new Vector3(xMovement, 0.0f, zMovement);
+
+        gameObject.transform.Translate(movement);
+
+        float xRotation = Input.GetAxis("Mouse X");
+        float yRotation = Input.GetAxis("Mouse Y");
+        gameObject.transform.Rotate(yRotation, xRotation, 0.0f);
+    }
+}
