7f123b960f63a124000c34d94e274970963a2580
Deibyyy <deibyjer@gmail.com>
2020-10-29 12:57:28 +0000

    Basic UI Added

---
+++ Assets/Scripts/UI_Controller.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI_Controller : MonoBehaviour {
+  [SerializeField]
+  private GameObject player;
+  [SerializeField]
+  private TextMeshProUGUI  healthUI;
+}
