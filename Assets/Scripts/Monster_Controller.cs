5a120cc6618e5df4120c50cb5cccca40078dddb4
billymoughton94 <billymoughton94@gmail.com>
2020-11-26 19:53:41 +0000

    Revamped the level design
    Also included a NavMesh component for monsters for movement. Audio files added - to be assigned

--- Assets/Scripts/Monster_Controller.cs
+++ Assets/Scripts/Monster_Controller.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Monster_Controller : MonoBehaviour {
     Rigidbody rb;
     Animator monsterAnim;
+    NavMeshAgent nav;
 
     private GameObject player;
     
@@ -17,6 +20,7 @@ public class Monster_Controller : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
+        nav = GetComponent<NavMeshAgent>();
         nextToPlayer = false;
         player = GameObject.FindGameObjectWithTag("Player");
         rb = GetComponent<Rigidbody>();
@@ -24,44 +28,40 @@ public class Monster_Controller : MonoBehaviour {
         distanceFromPlayer = Vector3.Distance(transform.position, player.transform.position);
     }
 
-    // Update is called once per frame
-    void FixedUpdate() {
+    private void Update()
+    {
+        chasePlayer();
+    }
+
+    private void chasePlayer()
+    {
         distanceFromPlayer = Vector3.Distance(transform.position, player.transform.position);
-        rotateTowardsPlayer();
-        moveTowardsPlayer();
-    }
 
-
-    // ROTATE TOWARDS PLAYER IF WITHIN AGGRO DISTANCE
-    private void rotateTowardsPlayer() {
-        if(distanceFromPlayer <= aggroDistance) {
-            Vector3 direction = (player.transform.position - transform.position).normalized;
-            Quaternion targetDirection = Quaternion.LookRotation(direction);
-            Quaternion deltaRotation = Quaternion.Slerp(transform.rotation, targetDirection, walkSpeed * Time.fixedDeltaTime);
-            rb.MoveRotation(Quaternion.Euler(new Vector3(rb.rotation.x, deltaRotation.eulerAngles.y, rb.rotation.z)));
-        }
-    }
-
-    private void moveTowardsPlayer() {
-        // ANIMATES MONSTER AND MOVES TOWARDS PLAYER IF IN AGGRO ZONE AND ABOVE ATTACK ZONE
-        if ((distanceFromPlayer <= aggroDistance) && nextToPlayer == false) {
-            rb.velocity = transform.forward * walkSpeed;
+        // IF PLAYER IS WITHIN AGRRO RANGE OF MONSTER AND NOT NEXT TO THE MONSTER, MONSTER STARTS TO CHASE PLAYER
+        if (distanceFromPlayer <= aggroDistance && nextToPlayer == false)
+        {
+            nav.isStopped = false;
+            nav.SetDestination(player.transform.position);
             if (monsterAnim.GetFloat("InputZ") != 1.0f)
                 monsterAnim.SetFloat("InputZ", 1.0f);
-
         }
 
-        // SWITCHES TO IDLE POSITION IF NEXT TO PLAYER OR OUTSIDE OF AGGRO DISTANCE
-        if (distanceFromPlayer > aggroDistance || nextToPlayer == true) {
+
+        // IF MONSTER IS TOO FAR FROM PLAYER OR RIGHT NEXT TO PLAYER, STOP CHASING
+        if (distanceFromPlayer > aggroDistance || nextToPlayer == true)
+        {
+            nav.isStopped = true;
             if (monsterAnim.GetFloat("InputZ") != 0.0f)
                 monsterAnim.SetFloat("InputZ", 0.0f);
         }
-
     }
 
     // BEGINS TO ATTACK WHEN PLAYER COMES WITHIN RANGE1
-    private void OnCollisionEnter(Collision collision) {
-        if(collision.collider.tag == "Player" && nextToPlayer == false) {
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player" && nextToPlayer == false)
+        {
+
             nextToPlayer = true;
             attack = attackPlayer();
             StartCoroutine(attack);
@@ -69,8 +69,10 @@ public class Monster_Controller : MonoBehaviour {
     }
 
     // STOPS ATTACKING AND CONTINUES TO PURSUE PLAYER WHEN OUTSIDE RANGE
-    private void OnCollisionExit(Collision collision) {
-        if (collision.collider.tag == "Player") {
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
             Debug.Log("LEAVING MONSTER ATTACK RANGE");
             nextToPlayer = false;
             StopCoroutine(attack);
@@ -79,9 +81,12 @@ public class Monster_Controller : MonoBehaviour {
     }
 
     // ATTACKS PLAYER IF NEXT TO THEM
-    IEnumerator attackPlayer() {
-        while(true) { 
-            if(monsterAnim.GetBool("NextToPlayer") != true) {
+    IEnumerator attackPlayer()
+    {
+        while (true)
+        {
+            if (monsterAnim.GetBool("NextToPlayer") != true)
+            {
                 monsterAnim.SetBool("NextToPlayer", true);
                 yield return new WaitForSeconds(2);
                 monsterAnim.SetBool("NextToPlayer", false);
@@ -89,7 +94,8 @@ public class Monster_Controller : MonoBehaviour {
             yield return new WaitForFixedUpdate();
 
             // DOUBLE CHECKS THE ATTACK RANGE AFTER FIXED UPDATE BEFORE APPLYING HITPOINT DEDUCTION. IF HIT, GAME OVER
-            if (nextToPlayer) {
+            if (nextToPlayer)
+            {
                 Debug.Log("THE MONSTER ATTACKS THE PLAYER");
                 Game_Manager.endGame(EndScenario.GAMEOVER);
             }
