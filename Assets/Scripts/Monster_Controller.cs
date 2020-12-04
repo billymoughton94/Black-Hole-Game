b05ce1872be7d2d05da4e07e0bcfc34a9e817433
billymoughton94 <billymoughton94@gmail.com>
2020-12-04 00:06:55 +0000

    Primary and Monster Audio Added
    Particle system for smoke also added

--- Assets/Scripts/Monster_Controller.cs
+++ Assets/Scripts/Monster_Controller.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class Monster_Controller : MonoBehaviour {
-    Rigidbody rb;
     Animator monsterAnim;
     NavMeshAgent nav;
-
     private GameObject player;
-    
+
     private float distanceFromPlayer;
     public float aggroDistance;
+    public float attackDistance;
 
-    //private bool nextToPlayer;
-
+    private AudioSource audioSource;
+    public AudioClip[] audioQueue;
     IEnumerator attack;
 
     // Start is called before the first frame update
     void Start() {
         nav = GetComponent<NavMeshAgent>();
-        //nextToPlayer = false;
         player = GameObject.FindGameObjectWithTag("Player");
-        rb = GetComponent<Rigidbody>();
         monsterAnim = GetComponent<Animator>();
         distanceFromPlayer = Vector3.Distance(transform.position, player.transform.position);
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(playMonsterAudio());
     }
 
     private void Update()
     {
         chasePlayer();
+        
     }
 
     private void chasePlayer()
@@ -37,46 +37,45 @@ public class Monster_Controller : MonoBehaviour {
         distanceFromPlayer = Vector3.Distance(transform.position, player.transform.position);
 
         // IF PLAYER IS WITHIN AGRRO RANGE OF MONSTER AND NOT NEXT TO THE MONSTER, MONSTER STARTS TO CHASE PLAYER
-        if (distanceFromPlayer <= aggroDistance)
+        if (distanceFromPlayer <= aggroDistance && distanceFromPlayer > attackDistance)
         {
             nav.SetDestination(player.transform.position);
-            if (monsterAnim.GetFloat("InputZ") != 1.0f)
-                monsterAnim.SetFloat("InputZ", 1.0f);
+            if (monsterAnim.GetFloat("InputZ") != 0.25f)
+                monsterAnim.SetFloat("InputZ", 0.25f);
         }
 
-
         // IF MONSTER IS TOO FAR FROM PLAYER OR RIGHT NEXT TO PLAYER, STOP CHASING
-        if (distanceFromPlayer > aggroDistance || nav.isStopped)
+        if (distanceFromPlayer > aggroDistance || distanceFromPlayer <= attackDistance)
         {
-            //nav.isStopped = true;
             if (monsterAnim.GetFloat("InputZ") != 0.0f)
                 monsterAnim.SetFloat("InputZ", 0.0f);
         }
     }
 
     // BEGINS TO ATTACK WHEN PLAYER COMES WITHIN RANGE
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
-        if (collision.collider.tag == "Player")
+        if (collision.tag == "Player")
         {
+            Debug.Log("MONSTER IN ATTACK RANGE");
 
-            //nextToPlayer = true;
             attack = attackPlayer();
             StartCoroutine(attack);
         }
     }
 
     // STOPS ATTACKING AND CONTINUES TO PURSUE PLAYER WHEN OUTSIDE RANGE
-    private void OnCollisionExit(Collision collision)
+    private void OnTriggerExit(Collider collision)
     {
-        if (collision.collider.tag == "Player")
+        if (collision.tag == "Player")
         {
             Debug.Log("LEAVING MONSTER ATTACK RANGE");
-            //nextToPlayer = false;
             StopCoroutine(attack);
             monsterAnim.SetBool("NextToPlayer", false);
         }
     }
+
+
 
     // ATTACKS PLAYER IF NEXT TO THEM
     IEnumerator attackPlayer()
@@ -89,16 +88,29 @@ public class Monster_Controller : MonoBehaviour {
                 yield return new WaitForSeconds(2);
                 monsterAnim.SetBool("NextToPlayer", false);
             }
-            yield return new WaitForFixedUpdate();
-
+            yield return new WaitForSeconds(2.0f);
             // DOUBLE CHECKS THE ATTACK RANGE AFTER FIXED UPDATE BEFORE APPLYING HITPOINT DEDUCTION. IF HIT, GAME OVER
-            //if (nextToPlayer)
-            //{
-               // Debug.Log("THE MONSTER ATTACKS THE PLAYER");
-                Game_Manager.endGame(EndScenario.GAMEOVER);
-           // }
-
+            Debug.Log("THE MONSTER ATTACKS THE PLAYER");
+            Game_Manager.endGame(EndScenario.GAMEOVER);
             yield return new WaitForSeconds(5.0f);
+        }
+    }
+
+    IEnumerator playMonsterAudio()
+    {
+
+        while (true)
+        {
+            for(int i = 0; i < audioQueue.Length; i++)
+            {
+                audioSource.clip = audioQueue[i];
+                audioSource.Play();
+
+                while (audioSource.isPlaying)
+                {
+                    yield return null;
+                }
+            }
         }
     }
 }
