dbc07ec7387031a5d9c2fdbbb04541c91ab2c598
billymoughton94 <billymoughton94@gmail.com>
2020-12-05 17:30:20 +0000

    Revert "Revert "Revert "Revert "Inventory  v3""""
    This reverts commit 582466b22a2f041d873e2fdb58df1c9cd4f66ab8.

--- Assets/Scripts/Item_Controller.cs
+++ Assets/Scripts/Item_Controller.cs
@@ -2,21 +2,23 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Item_Controller : MonoBehaviour {
+public class Item_Controller : MonoBehaviour, IPointerEnterHandler ,IPointerExitHandler {
 
     private string name;
     private int amount;
     private bool useable;
 
+    [SerializeField]
     private TextMeshProUGUI UIText;
+    [SerializeField]
     private GameObject UIButton;
+    [SerializeField]
+    private GameObject desciptionPrefab;
 
-    void Start() {
-        UIText = transform. GetComponentInChildren<TextMeshProUGUI>();
-        UIButton = GameObject.Find("Button");
-    }
+    private GameObject descriptionPanel;
 
     public void setValues(string name, int amount, bool useable) {
         this.name = name;
@@ -28,5 +30,14 @@ public class Item_Controller : MonoBehaviour {
     private void updateItem() {
         UIText.text = name + ": " + amount;
         UIButton.SetActive(useable);
+    }
+    
+    public void OnPointerEnter(PointerEventData data) {
+        descriptionPanel = Instantiate(desciptionPrefab, Input.mousePosition + new Vector3(125f, 50f, 0), Quaternion.identity, transform);
+        descriptionPanel.GetComponent<Description_Controller>().setText(name);
+    }
+
+    public void OnPointerExit(PointerEventData data) {
+        Destroy(descriptionPanel);	
     }
 }
