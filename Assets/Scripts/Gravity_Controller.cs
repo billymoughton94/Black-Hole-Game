abc0a93426bb42bac1b1328f8e59fc7123ee5427
billymoughton94 <billymoughton94@gmail.com>
2020-12-05 17:22:29 +0000

    .

--- Assets/Scripts/Gravity_Controller.cs
+++ Assets/Scripts/Gravity_Controller.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using UnityEngine;
 
 public class Gravity_Controller : MonoBehaviour {
@@ -17,6 +18,27 @@ public class Gravity_Controller : MonoBehaviour {
     pullRadius = pullRadius + growRate * Time.fixedDeltaTime;
     // Loop through the objects within the radius
     foreach (Collider collider in Physics.OverlapSphere(transform.position, pullRadius)) {
+=======
+﻿using UnityEngine;
+
+public class Gravity_Controller : MonoBehaviour {
+  
+  // Radius to get objects to be pulled it
+  [SerializeField]
+  private float pullRadius = 500;
+  // The force by which the objects will be pulled
+  [SerializeField]
+  private float pullForce = 1;
+  // The rate the radius will grow
+  [SerializeField]
+  private float growRate = 20;
+
+  public void FixedUpdate() {
+    // Increase the radius with every update
+    pullRadius = pullRadius + growRate * Time.fixedDeltaTime;
+    // Loop through the objects within the radius
+    foreach (Collider collider in Physics.OverlapSphere(transform.position, pullRadius)) {
+>>>>>>> parent of 3221baa... Merge branch 'main' of https://github.com/billymoughton94/Black-Hole-Game into main
       Vector3 forceDirection = transform.position - collider.transform.position;
       // Apply the force to the object
       if (collider.GetComponent<Rigidbody>() != null)
