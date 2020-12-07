4b2b41973df75fb47e30d55a8ae156ab66029a00
Deibyyy <deibyjer@gmail.com>
2020-12-07 02:09:08 +0000

    Title Screen

---
+++ Assets/Scripts/Title_Controller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title_Controller : MonoBehaviour {
+
+    [SerializeField] 
+    private GameObject mainPanel;
+    [SerializeField] 
+    private GameObject instructionPanel;
+    [SerializeField] 
+    private GameObject introPanel;
+
+    private float readingTime = 5.0f;
+    private bool waited;
+
+    void Start() {
+        waited = false;
+    }
+    
+
+    public void startPressed() {
+        mainPanel.SetActive(false);
+        introPanel.SetActive(true);
+        StartCoroutine("LoadSceneAsync");
+    }
+
+    public void instructionsPressed() {
+        mainPanel.SetActive(false);
+        instructionPanel.SetActive(true);
+    }
+
+    public void backPressed() {
+        mainPanel.SetActive(true);
+        instructionPanel.SetActive(false);
+    }
+    
+    IEnumerator LoadSceneAsync() {
+        yield return new WaitForSeconds(3);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("SampleScene");
+        while (!asyncLoad.isDone) {
+            yield return null;
+        }
+    }
+
+}
